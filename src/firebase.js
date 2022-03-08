10223f927d9bd37dd8653479c30f988b033b0d76
Luiz Rodrigues Junior <junior.luiz182@gmail.com>
2022-03-08 11:26:40 -0300

    Iniciando o projeto

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+  apiKey: "AIzaSyCtcd2qHDTooManrIFTyjlL6rv9S6al2Zc",
+  authDomain: "clone-44946.firebaseapp.com",
+  projectId: "clone-44946",
+  storageBucket: "clone-44946.appspot.com",
+  messagingSenderId: "522546881815",
+  appId: "1:522546881815:web:8b12c3f4f4f8fec892c352",
+  measurementId: "G-1KEG2CEC29"
+};
